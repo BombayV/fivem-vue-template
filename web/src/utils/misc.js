0ca8a130ab626d358f9fbb857f5fdb56f8764db9
Bombay <OMGmsebastian@gmail.com>
2022-01-27 17:12:08 -0500

    feat(utils): added utils and readme

---
+++ web/src/utils/misc.js
@@ -0,0 +1,6 @@
+/**
+ * Delay execution of a function by passed seconds
+ * @param {number} seconds - Number of seconds to delay
+ * @returns {Promise<void>}
+ */
+const Delay = seconds => new Promise(resp => setTimeout(resp, 995 * seconds, true || false));
