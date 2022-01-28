cbdd5ca7221eef1769ea71fa3a53ecbf241511d1
BombayV <OMGmsebastian@gmail.com>
2022-01-27 23:54:58 -0500

    feat(src): main wrapper

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="nui-wrapper">
+    <div class="basic-block">
+
+    </div>
   </div>
   <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100vh;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#app {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 }
 </style>
---
+++ web/src/components/CustomButton.vue
@@ -0,0 +1,16 @@
+<template lang="en">
+  <div>
+
+  </div>
+</template>
+
+<<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'CustomButton',
+  setup() {
+    const data = ref([]);
+  }
+}
+<script/>
